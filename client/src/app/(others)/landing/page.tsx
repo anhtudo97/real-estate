51bdfcd6e88821658b825df435dec9613b0953b7
Alex Dou <anhtudo97@gmail.com>
2025-06-27 02:43:27 +0700

    feat: discover section landing

--- client/src/app/(others)/landing/page.tsx
+++ client/src/app/(others)/landing/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import HeroSection from './Hero.section';
+import FeaturesSection from './Features.section';
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <>
+      <HeroSection />
+      <FeaturesSection />
+    </>
+  );
 };
 
 export default Landing;
