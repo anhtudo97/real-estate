1c858f7400a02e5ab4b80c8f88aa0a3822023259
Alex Dou <anhtudo97@gmail.com>
2025-06-29 22:24:59 +0700

    feat: update landing page

--- client/src/app/(others)/landing/page.tsx
+++ client/src/app/(others)/landing/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import HeroSection from './Hero.section';
 import FeaturesSection from './Features.section';
+import DiscoverSection from './Discover.section';
+import CallToActionSection from './CallToAction.section';
 
 const Landing = () => {
   return (
     <>
       <HeroSection />
       <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
     </>
   );
 };
