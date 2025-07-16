c2590d6f6b35ff1d9fa01c403ef6075dc5b01d93
Alex Dou <anhtudo97@gmail.com>
2025-07-17 02:40:33 +0700

    feat(server): update tenant + manager api

--- server/src/routes/manager.route.ts
+++ server/src/routes/manager.route.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import { createManager, getManager } from '../controllers/manager.controller';
+import {
+  createManager,
+  getManager,
+  updateManager,
+} from '../controllers/manager.controller';
 
 const router = express.Router();
 
 router.get('/:cognitoId', getManager);
+router.put('/:cognitoId', updateManager);
 router.post('/', createManager);
 
 export default router;
--- server/src/routes/tenant.route.ts
+++ server/src/routes/tenant.route.ts
@@ -1,8 +1,13 @@
 import express from 'express';
-import { createTenant, getTenant } from '../controllers/tenant.controller';
+import {
+  createTenant,
+  getTenant,
+  updateTenant,
+} from '../controllers/tenant.controller';
 
 const router = express.Router();
 
 router.get('/:cognitoId', getTenant);
+router.put('/:cognitoId', updateTenant);
 router.post('/', createTenant);
 export default router;
