61742b0bf178b80af542c1ddff74b4545edd3843
Alex Dou <anhtudo97@gmail.com>
2025-07-17 02:45:50 +0700

    feat(client): update  api call

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,9 +1,31 @@
 import { clsx, type ClassValue } from 'clsx';
+import { toast } from 'sonner';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+type MutationMessages = {
+  success?: string;
+  error: string;
+};
+
+export const withToast = async <T>(
+  mutationFn: Promise<T>,
+  messages: Partial<MutationMessages>,
+) => {
+  const { success, error } = messages;
+
+  try {
+    const result = await mutationFn;
+    if (success) toast.success(success);
+    return result;
+  } catch (err) {
+    if (error) toast.error(error);
+    throw err;
+  }
+};
 
 export const createNewUserInDatabase = async (
   user: any,
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,4 @@
-import { createNewUserInDatabase } from '@/lib/utils';
+import { createNewUserInDatabase, withToast } from '@/lib/utils';
 import { Manager, Tenant } from '@/types/prismaTypes';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth';
@@ -16,7 +16,15 @@ export const api = createApi({
     },
   }),
   reducerPath: 'api',
-  tagTypes: ['Managers', 'Tenants'],
+  tagTypes: [
+    'Managers',
+    'Tenants',
+    'Properties',
+    'PropertyDetails',
+    'Leases',
+    'Payments',
+    'Applications',
+  ],
   endpoints: (build) => ({
     getAuthUser: build.query<User, void>({
       queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
@@ -56,6 +64,24 @@ export const api = createApi({
         } catch (error: any) {
           return { error: error.message || 'Could not fetch user data' };
         }
+      },
+    }),
+
+    updateTenantSettings: build.mutation<
+      Tenant,
+      { cognitoId: string } & Partial<Tenant>
+    >({
+      query: ({ cognitoId, ...updatedTenant }) => ({
+        url: `tenants/${cognitoId}`,
+        method: 'PUT',
+        body: updatedTenant,
+      }),
+      invalidatesTags: (result) => [{ type: 'Tenants', id: result?.id }],
+      async onQueryStarted(_, { queryFulfilled }) {
+        await withToast(queryFulfilled, {
+          success: 'Settings updated successfully!',
+          error: 'Failed to update settings.',
+        });
       },
     }),
   }),
