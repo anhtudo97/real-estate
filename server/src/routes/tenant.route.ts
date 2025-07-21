a4f402d82dce9c0a78e6b68ea6fcba738448611b
Alex Dou <anhtudo97@gmail.com>
2025-07-22 02:37:21 +0700

    feat(server): update api for tenant

--- server/src/routes/tenant.route.ts
+++ server/src/routes/tenant.route.ts
@@ -1,13 +1,20 @@
 import express from 'express';
 import {
+  addFavoriteProperty,
   createTenant,
+  getCurrentResidences,
   getTenant,
+  removeFavoriteProperty,
   updateTenant,
 } from '../controllers/tenant.controller';
 
 const router = express.Router();
 
-router.get('/:cognitoId', getTenant);
-router.put('/:cognitoId', updateTenant);
-router.post('/', createTenant);
+router.get("/:cognitoId", getTenant);
+router.put("/:cognitoId", updateTenant);
+router.post("/", createTenant);
+router.get("/:cognitoId/current-residences", getCurrentResidences);
+router.post("/:cognitoId/favorites/:propertyId", addFavoriteProperty);
+router.delete("/:cognitoId/favorites/:propertyId", removeFavoriteProperty);
+
 export default router;
