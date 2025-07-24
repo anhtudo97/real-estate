9b235e93ae6e578a3f42024a08924abf83fb7321
Alex Dou <anhtudo97@gmail.com>
2025-07-25 02:55:42 +0700

    feat(client): update config

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -53,3 +53,17 @@ export const createNewUserInDatabase = async (
 
   return createUserResponse;
 };
+
+export function cleanParams(params: Record<string, any>): Record<string, any> {
+  return Object.fromEntries(
+    Object.entries(params).filter(
+      (
+        [_, value] // eslint-disable-line @typescript-eslint/no-unused-vars
+      ) =>
+        value !== undefined &&
+        value !== "any" &&
+        value !== "" &&
+        (Array.isArray(value) ? value.some((v) => v !== null) : value !== null)
+    )
+  );
+}
--- client/src/state/index.ts
+++ client/src/state/index.ts
@@ -1,6 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const initialState = {};
+export interface FiltersState {
+  location: string;
+  beds: string;
+  baths: string;
+  propertyType: string;
+  amenities: string[];
+  availableFrom: string;
+  priceRange: [number, number] | [null, null];
+  squareFeet: [number, number] | [null, null];
+  coordinates: [number, number];
+}
+
+interface InitialStateTypes {
+  filters: FiltersState;
+  isFiltersFullOpen: boolean;
+  viewMode: "grid" | "list";
+}
+
+export const initialState: InitialStateTypes = {
+  filters: {
+    location: "Los Angeles",
+    beds: "any",
+    baths: "any",
+    propertyType: "any",
+    amenities: [],
+    availableFrom: "any",
+    priceRange: [null, null],
+    squareFeet: [null, null],
+    coordinates: [-118.25, 34.05],
+  },
+  isFiltersFullOpen: false,
+  viewMode: "grid",
+};
 
 export const globalSlice = createSlice({
   name: 'global',
@@ -8,6 +40,6 @@ export const globalSlice = createSlice({
   reducers: {},
 });
 
-export const {} = globalSlice.actions;
+export const { } = globalSlice.actions;
 
 export default globalSlice.reducer;
