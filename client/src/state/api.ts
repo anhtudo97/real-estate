66b764ac8dfe858ea3ceb1d2f0207f19bf2c115a
Alex Dou <anhtudo97@gmail.com>
2025-07-30 03:34:10 +0700

    feat: update logic

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,5 +1,6 @@
-import { createNewUserInDatabase, withToast } from '@/lib/utils';
-import { Manager, Tenant } from '@/types/prismaTypes';
+import { cleanParams, createNewUserInDatabase, withToast } from '@/lib/utils';
+import { FiltersState } from '@/state';
+import { Manager, Property, Tenant } from '@/types/prismaTypes';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth';
 
@@ -102,7 +103,45 @@ export const api = createApi({
         });
       },
     }),
+
+    // property related endpoints
+    getProperties: build.query<
+      Property[],
+      Partial<FiltersState> & { favoriteIds?: number[]; }
+    >({
+      query: (filters) => {
+        const params = cleanParams({
+          location: filters.location,
+          priceMin: filters.priceRange?.[0],
+          priceMax: filters.priceRange?.[1],
+          beds: filters.beds,
+          baths: filters.baths,
+          propertyType: filters.propertyType,
+          squareFeetMin: filters.squareFeet?.[0],
+          squareFeetMax: filters.squareFeet?.[1],
+          amenities: filters.amenities?.join(","),
+          availableFrom: filters.availableFrom,
+          favoriteIds: filters.favoriteIds?.join(","),
+          latitude: filters.coordinates?.[1],
+          longitude: filters.coordinates?.[0],
+        });
+
+        return { url: "properties", params };
+      },
+      providesTags: (result) =>
+        result
+          ? [
+            ...result.map(({ id }) => ({ type: "Properties" as const, id })),
+            { type: "Properties", id: "LIST" },
+          ]
+          : [{ type: "Properties", id: "LIST" }],
+      async onQueryStarted(_, { queryFulfilled }) {
+        await withToast(queryFulfilled, {
+          error: "Failed to fetch properties.",
+        });
+      },
+    }),
   }),
 });
 
-export const { useGetAuthUserQuery, useUpdateTenantSettingsMutation, useUpdateManagerSettingsMutation } = api;
+export const { useGetAuthUserQuery, useUpdateTenantSettingsMutation, useUpdateManagerSettingsMutation, useGetPropertiesQuery } = api;
